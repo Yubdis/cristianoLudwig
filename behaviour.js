f9611d645217a5c6e1e6530f3f39186b96bb16f7
Simon <simontsui85@gmail.com>
2023-02-25 21:34:17 -0300

    added scroll behaviour

---
+++ behaviour.js
@@ -0,0 +1,24 @@
+<script>
+const debounce = (fn) => {
+    let frame;
+
+    return (...params) => {
+        if (frame) {
+            cancelAnimationFrame(frame);
+        }
+
+        frame = requestAnimationFrame(() => {
+            fn(...params);
+        });
+    };
+};
+
+const storeScroll = () => {
+    document.documentElement.dataset.scroll = window.scrollY;
+};
+
+storeScroll();
+
+document.addEventListener('scroll', debounce(storeScroll), { passive: true })
+
+</script>
